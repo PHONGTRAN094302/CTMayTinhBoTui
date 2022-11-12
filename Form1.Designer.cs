4dc4f5e6bba8cb7926d8c44f938a28481d48acd2
Your Name <you@example.com>
2022-11-12 21:01:07 +0700

    C# Winform

---
+++ Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace MayTinhBoTui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.bt_1 = new System.Windows.Forms.Button();
+            this.bt_2 = new System.Windows.Forms.Button();
+            this.bt_8 = new System.Windows.Forms.Button();
+            this.bt_3 = new System.Windows.Forms.Button();
+            this.bt_0 = new System.Windows.Forms.Button();
+            this.bt_4 = new System.Windows.Forms.Button();
+            this.bt_6 = new System.Windows.Forms.Button();
+            this.bt_7 = new System.Windows.Forms.Button();
+            this.bt_5 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.bt_9 = new System.Windows.Forms.Button();
+            this.bt_xoa = new System.Windows.Forms.Button();
+            this.bt_bang = new System.Windows.Forms.Button();
+            this.bt_cong = new System.Windows.Forms.Button();
+            this.bt_chia = new System.Windows.Forms.Button();
+            this.bt_tru = new System.Windows.Forms.Button();
+            this.bt_nhan = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(301, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // bt_1
+            // 
+            this.bt_1.Location = new System.Drawing.Point(42, 69);
+            this.bt_1.Name = "bt_1";
+            this.bt_1.Size = new System.Drawing.Size(50, 50);
+            this.bt_1.TabIndex = 1;
+            this.bt_1.Text = "1";
+            this.bt_1.UseVisualStyleBackColor = true;
+            // 
+            // bt_2
+            // 
+            this.bt_2.Location = new System.Drawing.Point(128, 69);
+            this.bt_2.Name = "bt_2";
+            this.bt_2.Size = new System.Drawing.Size(50, 50);
+            this.bt_2.TabIndex = 1;
+            this.bt_2.Text = "2";
+            this.bt_2.UseVisualStyleBackColor = true;
+            // 
+            // bt_8
+            // 
+            this.bt_8.Location = new System.Drawing.Point(128, 232);
+            this.bt_8.Name = "bt_8";
+            this.bt_8.Size = new System.Drawing.Size(50, 50);
+            this.bt_8.TabIndex = 1;
+            this.bt_8.Text = "8";
+            this.bt_8.UseVisualStyleBackColor = true;
+            // 
+            // bt_3
+            // 
+            this.bt_3.Location = new System.Drawing.Point(216, 69);
+            this.bt_3.Name = "bt_3";
+            this.bt_3.Size = new System.Drawing.Size(50, 50);
+            this.bt_3.TabIndex = 1;
+            this.bt_3.Text = "3";
+            this.bt_3.UseVisualStyleBackColor = true;
+            // 
+            // bt_0
+            // 
+            this.bt_0.Location = new System.Drawing.Point(128, 315);
+            this.bt_0.Name = "bt_0";
+            this.bt_0.Size = new System.Drawing.Size(50, 50);
+            this.bt_0.TabIndex = 1;
+            this.bt_0.Text = "0";
+            this.bt_0.UseVisualStyleBackColor = true;
+            // 
+            // bt_4
+            // 
+            this.bt_4.Location = new System.Drawing.Point(44, 143);
+            this.bt_4.Name = "bt_4";
+            this.bt_4.Size = new System.Drawing.Size(50, 50);
+            this.bt_4.TabIndex = 1;
+            this.bt_4.Text = "4";
+            this.bt_4.UseVisualStyleBackColor = true;
+            // 
+            // bt_6
+            // 
+            this.bt_6.Location = new System.Drawing.Point(216, 143);
+            this.bt_6.Name = "bt_6";
+            this.bt_6.Size = new System.Drawing.Size(50, 50);
+            this.bt_6.TabIndex = 1;
+            this.bt_6.Text = "6";
+            this.bt_6.UseVisualStyleBackColor = true;
+            // 
+            // bt_7
+            // 
+            this.bt_7.Location = new System.Drawing.Point(44, 232);
+            this.bt_7.Name = "bt_7";
+            this.bt_7.Size = new System.Drawing.Size(50, 50);
+            this.bt_7.TabIndex = 1;
+            this.bt_7.Text = "7";
+            this.bt_7.UseVisualStyleBackColor = true;
+            // 
+            // bt_5
+            // 
+            this.bt_5.Location = new System.Drawing.Point(128, 143);
+            this.bt_5.Name = "bt_5";
+            this.bt_5.Size = new System.Drawing.Size(50, 50);
+            this.bt_5.TabIndex = 1;
+            this.bt_5.Text = "5";
+            this.bt_5.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(-125, 313);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(50, 50);
+            this.button10.TabIndex = 1;
+            this.button10.Text = "1";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // bt_9
+            // 
+            this.bt_9.Location = new System.Drawing.Point(216, 232);
+            this.bt_9.Name = "bt_9";
+            this.bt_9.Size = new System.Drawing.Size(50, 50);
+            this.bt_9.TabIndex = 1;
+            this.bt_9.Text = "9";
+            this.bt_9.UseVisualStyleBackColor = true;
+            // 
+            // bt_xoa
+            // 
+            this.bt_xoa.Location = new System.Drawing.Point(44, 315);
+            this.bt_xoa.Name = "bt_xoa";
+            this.bt_xoa.Size = new System.Drawing.Size(50, 50);
+            this.bt_xoa.TabIndex = 1;
+            this.bt_xoa.Text = "C";
+            this.bt_xoa.UseVisualStyleBackColor = true;
+            this.bt_xoa.Click += new System.EventHandler(this.bt_xoa_Click);
+            // 
+            // bt_bang
+            // 
+            this.bt_bang.Location = new System.Drawing.Point(216, 315);
+            this.bt_bang.Name = "bt_bang";
+            this.bt_bang.Size = new System.Drawing.Size(50, 50);
+            this.bt_bang.TabIndex = 1;
+            this.bt_bang.Text = "=";
+            this.bt_bang.UseVisualStyleBackColor = true;
+            this.bt_bang.Click += new System.EventHandler(this.bt_bang_Click);
+            // 
+            // bt_cong
+            // 
+            this.bt_cong.Location = new System.Drawing.Point(295, 69);
+            this.bt_cong.Name = "bt_cong";
+            this.bt_cong.Size = new System.Drawing.Size(50, 50);
+            this.bt_cong.TabIndex = 1;
+            this.bt_cong.Text = "+";
+            this.bt_cong.UseVisualStyleBackColor = true;
+            this.bt_cong.Click += new System.EventHandler(this.bt_cong_Click);
+            // 
+            // bt_chia
+            // 
+            this.bt_chia.Location = new System.Drawing.Point(295, 315);
+            this.bt_chia.Name = "bt_chia";
+            this.bt_chia.Size = new System.Drawing.Size(50, 50);
+            this.bt_chia.TabIndex = 1;
+            this.bt_chia.Text = "/";
+            this.bt_chia.UseVisualStyleBackColor = true;
+            this.bt_chia.Click += new System.EventHandler(this.bt_chia_Click);
+            // 
+            // bt_tru
+            // 
+            this.bt_tru.Location = new System.Drawing.Point(295, 143);
+            this.bt_tru.Name = "bt_tru";
+            this.bt_tru.Size = new System.Drawing.Size(50, 50);
+            this.bt_tru.TabIndex = 1;
+            this.bt_tru.Text = "-";
+            this.bt_tru.UseVisualStyleBackColor = true;
+            this.bt_tru.Click += new System.EventHandler(this.bt_tru_Click);
+            // 
+            // bt_nhan
+            // 
+            this.bt_nhan.Location = new System.Drawing.Point(295, 232);
+            this.bt_nhan.Name = "bt_nhan";
+            this.bt_nhan.Size = new System.Drawing.Size(50, 50);
+            this.bt_nhan.TabIndex = 1;
+            this.bt_nhan.Text = "*";
+            this.bt_nhan.UseVisualStyleBackColor = true;
+            this.bt_nhan.Click += new System.EventHandler(this.bt_nhan_Click);
+            // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(-214, 486);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(50, 50);
+            this.button19.TabIndex = 1;
+            this.button19.Text = "1";
+            this.button19.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 405);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.bt_7);
+            this.Controls.Add(this.bt_tru);
+            this.Controls.Add(this.bt_6);
+            this.Controls.Add(this.bt_4);
+            this.Controls.Add(this.bt_xoa);
+            this.Controls.Add(this.bt_0);
+            this.Controls.Add(this.bt_5);
+            this.Controls.Add(this.bt_chia);
+            this.Controls.Add(this.bt_nhan);
+            this.Controls.Add(this.bt_bang);
+            this.Controls.Add(this.bt_cong);
+            this.Controls.Add(this.bt_9);
+            this.Controls.Add(this.bt_3);
+            this.Controls.Add(this.bt_8);
+            this.Controls.Add(this.bt_2);
+            this.Controls.Add(this.bt_1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button bt_1;
+        private System.Windows.Forms.Button bt_2;
+        private System.Windows.Forms.Button bt_8;
+        private System.Windows.Forms.Button bt_3;
+        private System.Windows.Forms.Button bt_0;
+        private System.Windows.Forms.Button bt_4;
+        private System.Windows.Forms.Button bt_6;
+        private System.Windows.Forms.Button bt_7;
+        private System.Windows.Forms.Button bt_5;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button bt_9;
+        private System.Windows.Forms.Button bt_xoa;
+        private System.Windows.Forms.Button bt_bang;
+        private System.Windows.Forms.Button bt_cong;
+        private System.Windows.Forms.Button bt_chia;
+        private System.Windows.Forms.Button bt_tru;
+        private System.Windows.Forms.Button bt_nhan;
+        private System.Windows.Forms.Button button19;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace MayTinhBoTui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            add_Click();
+            list = new List<string>();
+        }
+        List<Button> buttons;
+        List<string> list;
+        private void enableButton()
+        {
+            bt_cong.Enabled = true;
+            bt_tru.Enabled = true;
+            bt_nhan.Enabled = true;
+            bt_chia.Enabled = true;
+        }
+        private void disableButton()
+        {
+            bt_cong.Enabled = false;
+            bt_tru.Enabled = false;
+            bt_nhan.Enabled = false;
+            bt_chia.Enabled = false;
+        }
+        private void add_Click()
+        {
+            buttons = new List<Button>() { bt_0, bt_1, bt_2, bt_3, bt_4, bt_5, bt_6, bt_7, bt_8, bt_9 };
+            foreach(var item in buttons)
+            {
+                item.Click += new EventHandler(button_Click);
+            }
+        }
+        private void button_Click(object sender , EventArgs e)
+        {
+            //string textButton = ((Button)sender).Text;// dung tuong minh
+            string textButton = (sender as Button).Text;// dung as
+            textBox1.Text += textButton;
+        }
+        private void sum()
+        {
+            if(list.Contains("+"))
+            {
+                list.Remove("+");
+                double s = Convert.ToDouble(list[0]) + Convert.ToDouble(list[1]);
+                textBox1.Text = s.ToString();
+            }
+        }
+        private void sub()
+        {
+            if (list.Contains("-"))
+            {
+                list.Remove("-");
+                double s = Convert.ToDouble(list[0]) - Convert.ToDouble(list[1]);
+                textBox1.Text = s.ToString();
+            }
+        }
+        private void mul()
+        {
+            if (list.Contains("*"))
+            {
+                list.Remove("*");
+                double s = Convert.ToDouble(list[0]) * Convert.ToDouble(list[1]);
+                textBox1.Text = s.ToString();
+            }
+        }
+        private void div()
+        {
+            if (list.Contains("/"))
+            {
+                list.Remove("/");
+                double s = Convert.ToDouble(list[0]) / Convert.ToDouble(list[1]);
+                textBox1.Text = s.ToString();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show ("Bạn có muốn thoát chương trình không?","Thông Báo" , MessageBoxButtons.YesNo , MessageBoxIcon.Question);
+        }
+        private void bt_cong_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != "")
+            {
+                list.Add(textBox1.Text);
+                list.Add(bt_cong.Text);
+                textBox1.Text = "";
+                disableButton();
+            }
+        }
+        private void bt_bang_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != "")
+            {
+                list.Add(textBox1.Text);
+                sum();
+                sub();
+                mul();
+                div();
+                enableButton();
+                list.Clear();
+            }
+        }
+        private void bt_tru_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != "")
+            { 
+            list.Add(textBox1.Text);
+            list.Add(bt_tru.Text);
+            textBox1.Text = "";
+            disableButton();
+        }
+    }
+        private void bt_nhan_Click(object sender, EventArgs e)
+    { 
+            if(textBox1.Text != "")
+        {
+            list.Add(textBox1.Text);
+            list.Add(bt_nhan.Text);
+            textBox1.Text = "";
+            disableButton();
+        }
+    }
+        private void bt_chia_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != "")
+            {
+                list.Add(textBox1.Text);
+                list.Add(bt_chia.Text);
+                textBox1.Text = "";
+                disableButton();
+            }
+        }
+        private void bt_xoa_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.Focus();
+            list.Clear();
+            enableButton();
+        }
+    }
+}
